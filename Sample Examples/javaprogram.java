02954a79db78cce7b6f92400b909c048f25c7bc5
Arvind <arvind.s@gmail.com>
2022-08-22 13:34:15 +0530

    more programs added

---
+++ Sample Examples/javaprogram.java
@@ -0,0 +1,11 @@
+public class javaprogram   
+{ 
+
+    
+     
+public static void main(String[] args)   
+{  
+// using the max() method of Math class  
+System.out.print("The maximum number is: " + Math.max(9,7));  
+}  
+}  
