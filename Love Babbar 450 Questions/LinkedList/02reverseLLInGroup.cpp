f5821d318551832233a4d61fff2b89e205db9b21
Arvind Kumar <arvindkumar93258@gmail.com>
2022-09-08 10:49:29 +0530

    HR and Love Babbar Questions added

---
+++ Love Babbar 450 Questions/LinkedList/02reverseLLInGroup.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+
+Given a linked list of size N. The task is to reverse every k nodes (where k is an input to the function) in the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should be considered as a group and must be reversed (See Example 2 for clarification).
+
+Example 1:
+
+Input:
+LinkedList: 1->2->2->4->5->6->7->8
+K = 4
+Output: 4 2 2 1 8 7 6 5 
+
+*/
+
+
+
+
+
+
+int main(){
+
+
+
+
+    return 0;
+}
