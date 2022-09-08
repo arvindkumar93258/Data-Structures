4aff75c37ecf0cac4e6fdbcbfaffa13876a4fdf3
Arvind Kumar <arvindkumar93258@gmail.com>
2022-09-08 10:49:29 +0530

    HR and Love Babbar Questions added

---
+++ Love Babbar 450 Questions/LinkedList/01reverseLL.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+Given pointer to the head node of a linked list, the task is to reverse the linked list. We need to reverse the list by changing the links between nodes.
+
+Examples: 
+
+Input: Head of following linked list 
+1->2->3->4->NULL 
+Output: Linked list should be changed to, 
+4->3->2->1->NULL
+
+*/
+
+struct Node{
+    int val;
+    Node* next;
+};
+
+
+
+
+
+int main(){
+
+
+
+
+
+    return 0;
+}
